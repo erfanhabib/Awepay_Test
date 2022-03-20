fed76dbc0c752179bb9727bba726bfe124b315f3
Erfan Habib <erfan.habib@imp-w002.impulsebdltd.lan>
2022-03-20 19:46:51 +0600

    first commit

---
+++ Awepay_Test/Data/AppUserList.cs
@@ -0,0 +1,13 @@
+﻿using Awepay_Test.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Awepay_Test.Data
+{
+    public static class AppUserList
+    {
+        public static List<AppUser> AppUsers = new List<AppUser>();
+    }
+}
